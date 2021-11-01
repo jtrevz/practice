3cef7845b07593c940584113768327c6859737c2
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-01 11:30:05 -0500

    array methods large sub /first commit?

---
+++ index.js
@@ -0,0 +1,23 @@
+const numsA = [1,1,0,1,1,1]
+
+var findMaxConsecutiveOnes = function(nums) {
+    let currentMax = 0;
+    // let maxArray = [];
+    let max = 0;
+    for (let i=0; i < nums.length; i++) {
+       if (nums[i] === 1){
+           currentMax++
+       } else {
+         if (currentMax > max) {
+             max = currentMax
+         }
+         currentMax = 0;
+       }
+        if (currentMax > max) {
+             max = currentMax
+         }
+    }
+    return max
+};
+
+console.log(findMaxConsecutiveOnes(numsA));
---
+++ largeSub.js
@@ -0,0 +1,46 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+// Example 2:
+
+// Input: s = "bbbbb"
+// Output: 1
+// Explanation: The answer is "b", with the length of 1.
+// Example 3:
+
+// Input: s = "pwwkew"
+// Output: 3
+// Explanation: The answer is "wke", with the length of 3.
+// Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+// Example 4:
+
+// Input: s = ""
+// Output: 0
+
+
+function largestSub (string) {
+    let currentStr = []
+    let longest = 0
+
+    for (let i = 0; i < string.length; i++) {
+        const currentPosition = currentStr.indexOf(string[i]);
+
+        if (currentPosition !== -1) {
+            currentStr.splice(0, currentPosition + 1)
+        }
+        
+        currentStr.push(string[i]);
+
+        longest =  Math.max(longest, currentStr.length)    
+    }
+    return longest
+}
+
+console.log(largestSub("bbbbb"));
+
