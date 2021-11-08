5417f9d27ac2344e00f101a116f3c49b932e9686
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-08 15:47:47 -0600

    starting conditions for zigzag

---
+++ zigzag.js
@@ -0,0 +1,19 @@
+var convert = function(s, numRows) {
+    if (numRows === 1) {
+        return s;
+    }
+
+    let newString = '';
+
+    let diff = (numRows * 2) - 2;
+
+    // for ()
+
+
+
+
+
+
+}
+
+
