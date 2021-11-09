53890d937063af7e21d16b7eed95020991ad16e4
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-09 10:34:11 -0600

    zigzag solved

--- zigzag.js
+++ zigzag.js
@@ -5,15 +5,22 @@ var convert = function(s, numRows) {
 
     let newString = '';
 
-    let diff = (numRows * 2) - 2;
+    let key = (numRows * 2) - 2;
 
-    // for ()
+    for (let i = 0; i < numRows; i++) {
 
+        for (let x = i; x < s.length; x += key) {
+            newString += s[x];
 
+            if ((i != 0 ) && (i != numRows - 1) && (x + key - 2 * i) < s.length) {
+                newString += s[x + key - 2 * i];
+            }
+        }
+    }
 
-
-
+    return newString 
 
 }
 
 
+console.log(convert('PAYPALISHIRING', 4))
