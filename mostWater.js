260aa5903b00ef915f6610bbe1fb71db2204d115
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-11 10:42:01 -0600

    fixed: return outside of while loop :)

--- mostWater.js
+++ mostWater.js
@@ -6,13 +6,15 @@ var maxArea = function(height) {
     while (start < end) {
         if (height[start] < height[end]) {
             maxiArea = Math.max(maxiArea, height[start] * (end - start));
+            console.log('end is bigger loop' + start , end , maxiArea);
             start++;
         } else {
             maxiArea = Math.max(maxiArea, height[end] * (end - start));
+            console.log('start is bigger loop' + start , end , maxiArea);
             end--;
         }
-        return maxiArea
     }
+    return maxiArea
 }
 
-console.log(maxArea([1,2,1]));
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
