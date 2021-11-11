6bf51a9eac9aab64a7553f9301783e40ef231790
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-11 11:52:08 -0600

    trying hashtable

---
+++ intRoman.js
@@ -0,0 +1,30 @@
+var intToRoman = function(num) {
+    let numHold = num;
+    let numArray = [];
+    let counter = 0;
+    let romanObj = {
+        1000: 'M',
+        900: 'CM',
+        500: 'D',
+        400: 'CD',
+        100: 'C',
+        90: 'XC',
+        50: 'L',
+        40: 'XL',
+        10: '10',
+        9: 'IX',
+        5: 'V',
+        4: "IV",
+        1: '1'
+    }; 
+    let romanNum = '';
+    
+    while (numHold > 0) {
+        
+    }
+    console.log(numArray);
+
+
+}
+
+intToRoman(123)
