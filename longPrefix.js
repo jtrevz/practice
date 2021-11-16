3e6093349535fdb5af9434fba4fafb5e02a9202e
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-15 19:19:59 -0600

    answer accepted

---
+++ longPrefix.js
@@ -0,0 +1,12 @@
+var longestCommonPrefix = function(strs) {
+    if (!strs.length) return ''
+
+    for (let i = 0; i < strs[0].length; i++) {
+        if (!strs.every((piece) => piece[i] === strs[0][i])){
+            return strs[0].slice(0,i)
+        }
+    }
+    return strs[0];
+}
+
+console.log(longestCommonPrefix(["flower","flow","flight"]))
