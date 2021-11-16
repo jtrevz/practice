2ac5b36450eb4f755899bdde27271f94a4d03654
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-16 12:49:18 -0600

    problem worked out without checking

--- 3sum.js
+++ 3sum.js
@@ -4,9 +4,25 @@ let nums1 = [0];
 var threeSum = function (nums) {
     let solution = [];
 
+    if (nums == []) {
+        return solution
+    }
+
     for (var i = 0; i < nums.length; i++) {
-        let x = nums[i];
         
+        let x = (nums[i]) * -1;
+
+        for (let j = i + 1; j < nums.length; j++) {
+            let tempArray = []
+            let y = nums[j];
+            let z = nums[j + 1];
+
+            if ((y + z) == x) {
+                tempArray.push(x, y, z)
+                solution.push(tempArray)
+            }
+        }
 
     }
+    return solution;
 }
