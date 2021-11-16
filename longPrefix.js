2a2098a1b6e736970b15aeac1a969cb1691aa1ac
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-15 19:39:49 -0600

    3sum

--- longPrefix.js
+++ longPrefix.js
@@ -1,9 +1,12 @@
 var longestCommonPrefix = function(strs) {
-    if (!strs.length) return ''
+    if (!strs.length) return '' //return early * might make processing much faster
 
+
+    //below calculates each letter/charac of string in first position
     for (let i = 0; i < strs[0].length; i++) {
         if (!strs.every((piece) => piece[i] === strs[0][i])){
             return strs[0].slice(0,i)
+            //every evaluates each piece(string) and the position of character to see if they match
         }
     }
     return strs[0];
