fe820cc6f52731d64087aee949238396d526bb92
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-18 11:48:30 -0600

    new solution

--- 3sum.js
+++ 3sum.js
@@ -1,54 +1,58 @@
-let nums = [-1,0,1,2,-1,-4];
-let nums1 = [0];
+// let nums = [-1,0,1,2,-1,-4];
+// let nums1 = [0];
 
-const equal = (a,b) => {
-    a.every((v, r) => v === b[r])
-}
+// const equal = (a,b) => {
+//     a.every((v, r) => v === b[r])
+// }
 
-var threeSum = function (nums) {
+// var threeSum = function (nums) {
+//     let solution = [];
+
+//         if (nums == []) {
+//         return solution
+//     }
+
+//     for (var i = 0; i < nums.length; i++) {
+        
+//         let x = (nums[i]) * -1;
+
+//         for (let j = i + 1; j < nums.length; j++) {
+//             let tempArray = []
+//             let y = nums[j];
+//             let z = nums[j + 1];
+
+//             if (((y + z) == x)) {
+//                 tempArray.push((nums[i]), y, z);
+//                 tempArray.sort();
+//                 if (!(solution.find(array => array.every(function(e, index){
+//                     return e === tempArray[index]
+//                 })))) {
+//                     solution.push(tempArray);
+//                 }
+//             }
+//         }
+
+//     }
+//     return solution;
+// }
+
+
+
+var threeSum = function(array) {
+    array.sort(function(a, b) {return (a-b)})
+    console.log(array);
     let solution = [];
 
-        if (nums == []) {
-        return solution
-    }
+    for (let i = 0; i < array.length - 2; i++) {
+        if (array[i] != array[i - 1]) {
+            let l = i + 1;
+            let r = array.length - 1;
+            console.log(i, l, r);
 
-    for (var i = 0; i < nums.length; i++) {
-        
-        let x = (nums[i]) * -1;
-
-        for (let j = i + 1; j < nums.length; j++) {
-            let tempArray = []
-            let y = nums[j];
-            let z = nums[j + 1];
-
-            if (((y + z) == x)) {
-                tempArray.push((nums[i]), y, z);
-                tempArray.sort();
-                // let hi = ((solution.map(array => array.every(function(element, index) { 
-                //     return element === tempArray[index]
-                // }) )));
-
-                // console.log(hi);
-                // console.log('njandf;aiweug');
-                // console.log(solution.find(array => array.every(function(e, index){
-                //     return e === tempArray[index]
-                // })));
-                if (!(solution.find(array => array.every(function(e, index){
-                    return e === tempArray[index]
-                })))) {
-                    console.log("this might be the answer ")
-                    solution.push(tempArray);
-                }
-                // solution.push(tempArray);
-                // }
-
-
-            }
+            // while (left < right)
         }
-
     }
-    return solution;
-}
+};
 
 
-console.log(threeSum([0,0,-1,1]));
+console.log(threeSum([-1,0,1,2,-1,-4]));
