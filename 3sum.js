55c8c5d2bb42cb2cf5c772079ad943b4d4bd981a
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-18 13:09:45 -0600

    accepted solution

--- 3sum.js
+++ 3sum.js
@@ -38,20 +38,44 @@
 
 
 
-var threeSum = function(array) {
-    array.sort(function(a, b) {return (a-b)})
-    console.log(array);
+var threeSum = function(nums) {
+    if (nums == []) {
+        return solution
+    }
+    
+    nums.sort(function(a, b) {return (a-b)})
     let solution = [];
 
-    for (let i = 0; i < array.length - 2; i++) {
-        if (array[i] != array[i - 1]) {
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (nums[i] != nums[i - 1]) {
             let l = i + 1;
-            let r = array.length - 1;
-            console.log(i, l, r);
+            let r = nums.length - 1;
 
-            // while (left < right)
+            while (l < r) {
+                console.log(nums[i], nums[l],  nums[r]);
+                const tempRes = nums[i] + nums[l] + nums[r];
+                console.log(tempRes);
+                if (tempRes === 0) {
+                    solution.push([nums[i], nums[l], nums[r]])
+                    while(nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    while(nums[l] == nums[l + 1]) {
+                        l++;
+                    }
+                    r--;
+                    l++;
+                }
+                else if (tempRes < 0) {
+                    l++
+                }
+                else if (tempRes > 0) {
+                    r--;
+                }
+            }
         }
     }
+    return solution;
 };
 
 
