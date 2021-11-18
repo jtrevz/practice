eeecd0947ebf755601c9870d66a4c2a6eaecb719
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-18 14:19:20 -0600

    similar to 3sum

--- 3sumclose.js
+++ 3sumclose.js
@@ -1,51 +1,38 @@
-// var threeSum = function(nums) {
-//     if (nums == []) {
-//         return solution
-//     }
-    
-//     nums.sort(function(a, b) {return (a-b)})
-//     let solution = [];
 
-//     for (let i = 0; i < nums.length - 2; i++) {
-//         if (nums[i] != nums[i - 1]) {
-//             let l = i + 1;
-//             let r = nums.length - 1;
-
-//             while (l < r) {
-//                 console.log(nums[i], nums[l],  nums[r]);
-//                 const tempRes = nums[i] + nums[l] + nums[r];
-//                 console.log(tempRes);
-//                 if (tempRes === 0) {
-//                     solution.push([nums[i], nums[l], nums[r]])
-//                     while(nums[r] == nums[r - 1]) {
-//                         r--;
-//                     }
-//                     while(nums[l] == nums[l + 1]) {
-//                         l++;
-//                     }
-//                     r--;
-//                     l++;
-//                 }
-//                 else if (tempRes < 0) {
-//                     l++
-//                 }
-//                 else if (tempRes > 0) {
-//                     r--;
-//                 }
-//             }
-//         }
-//     }
-//     return solution;
-// };
 
 var threeSumClosest = function(nums, target) {
-    let closestSum = 0;
-
     if (nums.length === 3) {
         return nums.reduce((a,b)=> a + b, 0)
     }
 
     nums.sort(function(a, b) {return (a-b)})
+    let closestSum;
 
-    
+    for (let i = 0; i < nums.length - 2; i++) {
+        let l = i + 1;
+        let r = nums.length - 1;
+
+        while (l < r) {
+            const tempRes = nums[i] + nums[r] + nums[l];
+            let diff = target - tempRes
+            let diff1 = 0
+            if (diff == 0) {
+                return tempRes;
+            }
+            else if ((!closestSum)|| (Math.abs(diff1) > Math.abs(diff))){
+                closestSum = tempRes;
+                diff1 = diff // leave ++ till later
+            } 
+            if (diff > 0) {
+                r--;
+            } else if (diff < 0) {
+                l++;
+            }
+        }
+    }
+
+    return closestSum
+
 };
+
+console.log(threeSumClosest([-1,2,1,-4], 1));
