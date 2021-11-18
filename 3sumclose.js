480d1e149f87f05ca358d0c0a3e059e2e940f928
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-18 15:28:15 -0600

    playing around with fxn

--- 3sumclose.js
+++ 3sumclose.js
@@ -1,5 +1,39 @@
 
 
+// var threeSumClosest = function(nums, target) {
+//     if (nums.length === 3) {
+//         return nums.reduce((a,b)=> a + b, 0)
+//     }
+
+//     nums.sort(function(a, b) {return (a-b)})
+//     let closestSum = 1000000000;
+
+//     for (let i = 0; i < nums.length - 2; i++) {
+//         let l = i + 1;
+//         let r = nums.length - 1;
+
+//         while (l < r) {
+//             const tempRes = nums[i] + nums[r] + nums[l];
+//             if (tempRes === target) {
+//                 return tempRes;
+//             }
+//             if (Math.abs(target-tempRes) < Math.abs(target - closestSum)){
+//                 closestSum = tempRes;
+//             } 
+
+//             if (tempRes > target) {
+//                 r--;
+//             }
+//             else  {
+//                 l++;
+//             }
+//         }
+//     }
+
+//     return closestSum
+
+// };
+
 var threeSumClosest = function(nums, target) {
     if (nums.length === 3) {
         return nums.reduce((a,b)=> a + b, 0)
@@ -14,10 +48,13 @@ var threeSumClosest = function(nums, target) {
 
         while (l < r) {
             const tempRes = nums[i] + nums[r] + nums[l];
+            console.log(nums[i], nums[l], nums[r]);
+            const diff = target-tempRes
             if (tempRes === target) {
-                return tempRes;
+                closestSum = tempRes;
+                return closestSum
             }
-            if (Math.abs(target-tempRes) < Math.abs(target - closestSum)){
+            if (Math.abs(diff) < Math.abs(target - closestSum)){
                 closestSum = tempRes;
             } 
 
@@ -36,4 +73,4 @@ var threeSumClosest = function(nums, target) {
 
 
 
-console.log(threeSumClosest([-1,2,1,-4], 1));
+console.log(threeSumClosest([0,2,1,-3], 1));
