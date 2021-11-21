1ef8691d8f1ae05b50634c0258026838c0e6e2f2
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-20 23:36:30 -0600

    map pbject done

--- letterCombo.js
+++ letterCombo.js
@@ -1,3 +1,16 @@
+const numberMap = new Map([
+    [2, ['a', 'b', 'c']],
+    [3, ['d', 'e', 'f']],
+    [4, ['g', 'h', 'i']],
+    [5, ['j', 'k', 'l']],
+    [6, ['m', 'n', 'o']],
+    [7, ['p', 'q', 'r', 's']],
+    [8, ['t', 'u', 'v']],
+    [9, ['w', 'x', 'y', 'z']]
+])
+
 var letterCombinations = function(digits) {
-    
+    return numberMap.get(2)
 };
+
+console.log(letterCombinations(2))
