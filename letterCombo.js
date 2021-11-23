d9ac0870338d96a033475e0001abeb62e7410db9
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2021-11-22 18:35:19 -0600

    sups confused

--- letterCombo.js
+++ letterCombo.js
@@ -1,31 +1,42 @@
-const numberMap = new Map([
-    ['2', ['a', 'b', 'c']],
-    ['3', ['d', 'e', 'f']],
-    ['4', ['g', 'h', 'i']],
-    ['5', ['j', 'k', 'l']],
-    ['6', ['m', 'n', 'o']],
-    ['7', ['p', 'q', 'r', 's']],
-    ['8', ['t', 'u', 'v']],
-    ['9', ['w', 'x', 'y', 'z']]
-])
+const numberMap = {
+    '2': ['a','b','c'],
+    '3' :['d','e','f'],
+    '4': ['g','h','i'],
+    '5': ['j','k','l'],
+    '6': ['m','n','o'],
+    '7': ['p','q','r','s'],
+    '8': ['t','u','v'],
+    '9': ['w','x','y','z']
+}
 
 var letterCombinations = function(digits) {
     if (digits === '') {
         return []
     }
+
     const result = [];
 
     const dfs = (i, digits, opt) => {
-        let char = numberMap.get(digits[i])
+        if (digits === '') {
+            return []
+        }
+        let character = numberMap[digits[i]]
     
+        console.log(character);
 
-        char.map(chars=>{
-            opt.push(chars);
-            dfs(1+i , digits, opt);
-            slate.pop();
-        })
+        for (let x=0; x < character.length; x++) {
+            opt.push(character[i]);
+            dfs(1 + i , digits, opt);
+            opt.pop();
+        }
+        // for(let char of character) {
+        //     opt.push(char);
+        //     dfs(1 + i , digits, opt);
+        //     slate.pop();
+        // }
     }
     dfs(0, digits, []);
+
 
     return result;
 };
