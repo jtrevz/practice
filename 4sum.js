5c0f3f535ea420af44fa4dd8d1e46ccec668f043
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-08 13:11:09 -0500

    new 4sumapproach

--- 4sum.js
+++ 4sum.js
@@ -1,46 +1,59 @@
-let fourSum = function (nums, target) {
-    let solution = [];
+// let fourSum = function (nums, target) {
+//     let solution = [];
 
-    //sort  
-    nums.sort(function(a,b) {return (a-b)})
+//     //sort  
+//     nums.sort(function(a,b) {return (a-b)})
 
-    for(let i = 0; i < nums.length -2; i++) {
-        //counters
-        if(nums[i] != nums[i - 1]) {
-            let l = i + 1;
-            let r = nums.length - 1;
+//     for(let i = 0; i < nums.length -2; i++) {
+//         //counters
+//         if(nums[i] != nums[i - 1]) {
+//             let l = i + 1;
+//             let r = nums.length - 1;
 
-            while (l < r) {
-                const sum = (nums[i] + nums [l] + nums [r])
-                console.log(`sum: ${sum}`);
-                const difference = target - sum;
-                console.log(`differnece: ${difference}`);
-                const n = nums.indexOf(difference, (l+1))
-                console.log(`n: ${n}`);
-                if((0 < n)&&(n < r)) {
-                    console.log(i , l, n, r);
-                    solution.push([nums[i], nums[l], nums[n], nums [r]])
-                    while(nums[r] == nums[r - 1]) {
-                        r--;
-                    }
-                    while(nums[l] == nums[l + 1]) {
-                        l++
-                    }
-                    r--;
-                    l++;
-                }
-                else if (sum < target) {
-                    l++
-                }
-                else if (sum > target) {
-                    r--;
-                }
-            }
+//             while (l < r) {
+//                 const sum = (nums[i] + nums [l] + nums [r])
+//                 console.log(`sum: ${sum}`);
+//                 const difference = target - sum;
+//                 console.log(`differnece: ${difference}`);
+//                 let n = nums.indexOf(difference)
+//                 console.log(`n: ${n}`);
+//                 if((0 < n) && (n < r)) {
+//                     console.log(i ,l, n, r);
+//                     solution.push([nums[i], nums[l], nums[n], nums [r]])
+//                     while(nums[r] == nums[r - 1]) {
+//                         r--;
+//                     }
+//                     while(nums[l] == nums[l + 1]) {
+//                         l++
+//                     }
+//                     r--;
+//                     l++;
+//                 }
+//                 else if (sum < target) {
+//                     l++
+//                 }
+//                 else if (sum > target) {
+//                     r--;
+//                 }
+//             }
+//         }
+//     }
+    
+//     return solution
+// };
+
+function fourSum(nums, target) {
+    const result = [];
+    if (nums.length < 4) {
+        return result;
+    }  
+
+    for (let i = 0; 1 < nums.length-3; i++) {
+        for (let j = i +1; j < nums.length-2; j++) {
+            
         }
     }
-    
-    return solution
-};
+}
 
 console.log(fourSum([2,2,2,2,2], 8))
 
