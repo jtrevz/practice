690a16f367b7737f08dfceb06bcd5403d8229bdb
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-08 12:38:53 -0500

    specific counter for 4sums, indexof

--- 4sum.js
+++ 4sum.js
@@ -6,7 +6,22 @@ let fourSum = function (nums, target) {
 
     for(let i = 0; i < nums.length -2; i++) {
         //counters
+        if(nums[i] != nums[i - 1]) {
+            let l = i + 1;
+            let r = nums.length - 1;
 
+            while (l < r) {
+                const difference = target - (nums[i] + nums [l] + nums [r]);
+                const n = nums.indexOf(difference, (l+1))
+                if(0 < n < r) {
+                    solution.push([nums[i], nums[l], nums[n], nums [r]])
+                    while(nums[r] == nums[r - 1]) {
+                        r--;
+                    }
+                    while
+                }
+            }
+        }
         //while
 
             //if(tempRes = target)
---
+++ indexof.js
@@ -0,0 +1,6 @@
+let x = [2,2,-1,4,8,2];
+
+function indexof () {
+    console.log(x.indexOf(9));
+}
+indexof()
