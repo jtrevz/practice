fd95c50f6ff497677afb8eefb3328e9c7199e9af
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-07 21:49:42 -0500

    4sum plan

---
+++ 4sum.js
@@ -0,0 +1,16 @@
+let fourSum = function (nums, target) {
+    let solution = [];
+
+    //sort  
+    nums.sort(function(a,b) {return (a-b)})
+
+    for(let i = 0; i < nums.length -2; i++) {
+        //counters
+
+        //while
+
+            //if(tempRes = target)
+    }
+    
+    return solution
+};
