3cec3dc27ec50cf1dd6d1c83b1ebde5dae1d40dd
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-13 11:20:57 -0500

    node version

--- mergeList.js
+++ mergeList.js
@@ -28,26 +28,23 @@ console.log(mergeTwo([],[]));
 console.log(mergeTwo([],[0]));
 
 let mergedNode = (l1,l2) => {
-    let mergedList = [];
-    let counter1 = 0;
-    let counter2 = 0;
+    let mergedList = new NodeList;
     
-    while(l1[counter1] && l2[counter2]) {
-        if (l1[counter1] < l2[counter2]) {
-            mergedList.push(l1[counter1])
-            counter1++
+    while(l1 && l2) {
+        if (l1.val < l2.val) {
+            mergedList.next = l1
+            l1 = l1.next
         } else{
-            mergedList.push(l2[counter2])
-            counter2++
+            mergedList.next = l2
+            l2 = l2.next
         }
+        mergedList = mergedList.next;
     }
-    if(counter1 < l1.length) {
-        mergedList.push(l1[counter1])
-            counter1++
+    if(l1) {
+        mergedList.next = l1
     }
-    if(counter2 < l2.length) {
-        mergedList.push(l2[counter2])
-            counter2++
+    if(l2) {
+        mergedList.next = l2
     }
     return mergedList 
 }
