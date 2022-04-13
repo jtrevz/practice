9a8afcd06587e59ffe4b16f4efbb52d0c755072c
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2022-04-13 10:25:38 -0500

    stack

--- parenthesis.js
+++ parenthesis.js
@@ -1,21 +1,25 @@
 let isValid = function(s) {
-    let pArray = s.split('')
     let i = 0;
     let stack = [];
+    const key = '() {} []'
 
-    while(i < pArray.length) {
-        stack.push(pArray[i])
+    while(i < s.length) {
+        stack.push(s[i])
         i++;
+
+        let open = stack[stack.length - 2];
+        let closed = stack[stack.length - 1];
+
+        let compare = open + closed
+
+        if (key.includes(compare)) {
+            stack.splice(open, 2)
+        }
     }
-
-    let open = slack[slack.length - 2];
-    let closed = slack[slack.length - 1];
-
-
-    
+    return (stack.length === 0)
 }
 
-console.log(isValid("()"))
+console.log(isValid("()"), )
 
 console.log(isValid("()[]{}"))
 console.log(isValid("(]"))
