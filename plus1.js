de01c3949589d6312e1852e631deaabc0dbf7b49
Jenny Trevizo <jenny.trevizo2013@gmail.com>
2023-01-19 22:00:40 -0600

    initial set up

---
+++ plus1.js
@@ -0,0 +1,15 @@
+var plusOne = function(digits) {
+    let counter = digits.length - 1
+    let carry = 0
+    if (digits[digits.length - 1] < 9) {
+        return digits[digits.length - 1]++
+    } else {
+        while (counter >= 0 ) {
+            
+        }
+    }
+};
+
+console.log(plusOne([9]))
+
+console.log(plusOne([1,2,3]))
